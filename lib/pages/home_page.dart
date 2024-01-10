a41adb13c0a44427ce1a6f559482f8ea389aa514
Wilfred Marete <marete015@gmail.com>
2024-01-10 16:34:49 +0300

    Updated list view on the UI commit

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -14,201 +14,299 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.blue[800],
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.calendar_today,
+            ),
+            label: 'Calendar',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.person,
+            ),
+            label: 'Profile',
+          ),
+        ],
+      ),
       body: SafeArea(
-        child: Padding(
-          padding: EdgeInsets.all(25.0),
-          child: Column(
-            children: [
-              // ============= Greetings Row
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: Column(
                 children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
+                  // ============= Greetings Row
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      const Text(
-                        'Hi, Willy!',
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text(
+                            'Hi, Willy!',
+                            style: TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 8,
+                          ),
+                          Text(
+                            DateFormat.yMMMEd().format(DateTime.now()),
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.blue[200],
+                            ),
+                          ),
+                        ],
+                      ),
+
+                      // Notification Icon
+                      Container(
+                        decoration: BoxDecoration(
+                          color: Colors.blue[600],
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        padding: const EdgeInsets.all(10),
+                        child: const Icon(
+                          Icons.notifications,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+
+                  const SizedBox(
+                    height: 40,
+                  ),
+
+                  // ============= Search Bar
+                  Container(
+                    padding: const EdgeInsets.all(12),
+                    decoration: BoxDecoration(
+                      color: Colors.blue[600],
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: const Row(
+                      children: [
+                        Icon(
+                          Icons.search,
+                          color: Colors.white,
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Expanded(
+                          child: TextField(
+                            decoration: InputDecoration(
+                              hintText: 'Search',
+                              border: InputBorder.none,
+                              hintStyle: TextStyle(
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 25,
+                  ),
+
+                  // ============= How do you feel?
+                  const Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'How do you feel?',
                         style: TextStyle(
-                          fontSize: 24,
+                          fontSize: 20,
                           fontWeight: FontWeight.bold,
                           color: Colors.white,
                         ),
                       ),
-                      const SizedBox(
-                        height: 8,
-                      ),
-                      Text(
-                        DateFormat.yMMMEd().format(DateTime.now()),
-                        style: TextStyle(
-                          fontSize: 16,
-                          color: Colors.blue[200],
-                        ),
+                      Icon(
+                        Icons.more_horiz,
+                        color: Colors.white,
                       ),
                     ],
                   ),
 
-                  // Notification Icon
-                  Container(
-                    decoration: BoxDecoration(
-                      color: Colors.blue[600],
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    padding: const EdgeInsets.all(10),
-                    child: const Icon(
-                      Icons.notifications,
-                      color: Colors.white,
-                    ),
+                  const SizedBox(
+                    height: 25,
                   ),
-                ],
-              ),
 
-              const SizedBox(
-                height: 40,
-              ),
-
-              // ============= Search Bar
-              Container(
-                padding: const EdgeInsets.all(12),
-                decoration: BoxDecoration(
-                  color: Colors.blue[600],
-                  borderRadius: BorderRadius.circular(12),
-                ),
-                child: const Row(
-                  children: [
-                    Icon(
-                      Icons.search,
-                      color: Colors.white,
-                    ),
-                    SizedBox(
-                      width: 10,
-                    ),
-                    Expanded(
-                      child: TextField(
-                        decoration: InputDecoration(
-                          hintText: 'Search',
-                          border: InputBorder.none,
-                          hintStyle: TextStyle(
-                            color: Colors.white,
+                  // ============= Mood Buttons
+                  const Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Column(
+                        children: [
+                          // Badly
+                          EmoticonFace(
+                            emoticonFace: '😫',
                           ),
-                        ),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Text(
+                            'Badly',
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
                       ),
-                    ),
-                  ],
-                ),
-              ),
-              const SizedBox(
-                height: 25,
-              ),
 
-              // ============= How do you feel?
-              const Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    'How do you feel?',
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                    ),
-                  ),
-                  Icon(
-                    Icons.more_horiz,
-                    color: Colors.white,
-                  ),
-                ],
-              ),
-
-              const SizedBox(
-                height: 25,
-              ),
-
-              // ============= Mood Buttons
-              const Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Column(
-                    children: [
-                      // Badly
-                      EmoticonFace(
-                        emoticonFace: '😫',
+                      //Fine
+                      Column(
+                        children: [
+                          // Badly
+                          EmoticonFace(
+                            emoticonFace: '😊',
+                          ),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Text(
+                            'Fine',
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
                       ),
-                      SizedBox(
-                        width: 10,
+                      //Well
+                      Column(
+                        children: [
+                          // Badly
+                          EmoticonFace(
+                            emoticonFace: '😁',
+                          ),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Text(
+                            'Well',
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
                       ),
-                      Text(
-                        'Badly',
-                        style: TextStyle(
-                          color: Colors.white,
-                        ),
-                      ),
-                    ],
-                  ),
-
-                  //Fine
-                  Column(
-                    children: [
-                      // Badly
-                      EmoticonFace(
-                        emoticonFace: '😊',
-                      ),
-                      SizedBox(
-                        width: 10,
-                      ),
-                      Text(
-                        'Fine',
-                        style: TextStyle(
-                          color: Colors.white,
-                        ),
-                      ),
-                    ],
-                  ),
-                  //Well
-                  Column(
-                    children: [
-                      // Badly
-                      EmoticonFace(
-                        emoticonFace: '😁',
-                      ),
-                      SizedBox(
-                        width: 10,
-                      ),
-                      Text(
-                        'Well',
-                        style: TextStyle(
-                          color: Colors.white,
-                        ),
-                      ),
-                    ],
-                  ),
-                  //Excellent
-                  Column(
-                    children: [
-                      // Badly
-                      EmoticonFace(
-                        emoticonFace: '😍',
-                      ),
-                      SizedBox(
-                        width: 10,
-                      ),
-                      Text(
-                        'Excellent',
-                        style: TextStyle(
-                          color: Colors.white,
-                        ),
+                      //Excellent
+                      Column(
+                        children: [
+                          // Badly
+                          EmoticonFace(
+                            emoticonFace: '😍',
+                          ),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Text(
+                            'Excellent',
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
                       ),
                     ],
                   ),
                 ],
               ),
-
-              Expanded(
-                child: Container(
-                  color: Colors.white,
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+            Expanded(
+              child: Container(
+                padding: const EdgeInsets.all(25),
+                color: Colors.grey[200],
+                child: Center(
+                  child: Column(
+                    children: [
+                      const Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'Exercises',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                            ),
+                          ),
+                          Icon(Icons.more_horiz),
+                        ],
+                      ),
+                      // ============ Listview of exercises
+                      Container(
+                        padding: const EdgeInsets.all(16),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(16),
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Row(
+                              children: [
+                                Container(
+                                  child:
+                                      Icon(Icons.favorite, color: Colors.white),
+                                  padding: const EdgeInsets.all(16),
+                                  decoration: BoxDecoration(
+                                    color: Colors.orange,
+                                    borderRadius: BorderRadius.circular(12),
+                                  ),
+                                ),
+                                const SizedBox(
+                                  width: 10,
+                                ),
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      'Breathing Exercise',
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 16,
+                                      ),
+                                    ),
+                                    const SizedBox(
+                                      height: 5,
+                                    ),
+                                    Text(
+                                      '15 mins',
+                                      style: TextStyle(
+                                        color: Colors.grey[600],
+                                        fontSize: 14,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                            Icon(Icons.more_horiz),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
